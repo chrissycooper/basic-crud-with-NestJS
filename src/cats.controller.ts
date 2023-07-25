dad2984109f7516a71982c1892f8e3da2182c420
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-07-25 17:41:53 -0500

    Add basic POST route

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Req } from "@nestjs/common";
+import { Controller, Get, Req, Post } from "@nestjs/common";
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'this action creates a new cat';
+  }
+
   @Get()
   findAll(@Req() request: Request): string {
-    return 'This action returns all cats'
+    return 'This action returns all cats';
   }
 }
