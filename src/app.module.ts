8254a09831779f9e7239b787b83c285847ca56c5
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-07-25 17:18:06 -0500

    Create basic GET all cats route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats.controller';
 
 //the root module for the application
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
