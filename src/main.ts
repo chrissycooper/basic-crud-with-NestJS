9e9718910a6c3c80b4816702e5deb42ac1d131f4
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-07-25 13:41:09 -0500

    Add note on NestFactory

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core'; //the core NestFactory class exposes static methods that allow creating an app instance
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule); //the create method returns an application object which fulfills the INestApplication interface
   await app.listen(3000);
 }
 bootstrap();
