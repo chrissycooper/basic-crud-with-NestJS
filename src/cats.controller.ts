9d2eca0a2060e3a048294cd862f69564997c4877
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-07-26 17:38:00 -0500

    Adjust response object header

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Req, Post, HttpCode } from "@nestjs/common";
+import { Controller, Get, Req, Post, HttpCode, Header } from "@nestjs/common";
 import { Request } from "express";
 
 @Controller('cats')
 export class CatsController {
   @Post()
   @HttpCode(204)
+  @Header('Cache-Control', 'none')
   create(): string {
     return 'this action creates a new cat';
   }
