a9dc467382072d20ff8fbb04d16907313554e2b2
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-07-28 18:08:43 -0500

    Add example with hostParams

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { CatsController } from './cats.controller';
 import { NestDocsController } from './nest-docs/nest-docs.controller';
 import { AdminControllerController } from './admin-controller/admin.controller';
+import { AccountController } from './account/account.controller';
 
 //the root module for the application
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, NestDocsController, AdminControllerController],
+  controllers: [AppController, CatsController, NestDocsController, AdminControllerController, AccountController],
   providers: [AppService],
 })
 export class AppModule {}
