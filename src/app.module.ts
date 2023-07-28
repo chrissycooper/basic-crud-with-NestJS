951acc5bc00a13e264855b7f9d4258ef298dcf7d
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-07-28 17:54:18 -0500

    Implement host option to require specific HTTP address in request

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats.controller';
 import { NestDocsController } from './nest-docs/nest-docs.controller';
+import { AdminControllerController } from './admin-controller/admin.controller';
 
 //the root module for the application
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, NestDocsController],
+  controllers: [AppController, CatsController, NestDocsController, AdminControllerController],
   providers: [AppService],
 })
 export class AppModule {}
