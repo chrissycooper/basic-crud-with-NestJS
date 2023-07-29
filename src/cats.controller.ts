d7a6ab925d9f8ee2944836b0eef4e0f7317ef872
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-07-29 15:05:42 -0500

    Create js object as a type statement for the DTO for cats controller post route

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Req, Post, HttpCode, Header, Param } from "@nestjs/common";
+import { Controller, Get, Req, Post, HttpCode, Header, Param, Body } from "@nestjs/common";
 import { Request } from "express";
+import { CreateCatDto } from "./create-cat.dto";
 
 @Controller('cats')
 export class CatsController {
   @Post()
   @HttpCode(204)
   @Header('Cache-Control', 'none')
-  create(): string {
+  //we're using a JS class to type this DTO (Data Transfer Object) instead of a TS interface because the js class will be available throughout the life of the program, and interfaces are removed during the transpilation so Nest cannot refer to them at run time. There are some possiblities Pipes can give us if they have access to the metatype of the variable at runtime.
+  async create(@Body() createCatdto: CreateCatDto) {
     return 'this action creates a new cat';
   }
 
