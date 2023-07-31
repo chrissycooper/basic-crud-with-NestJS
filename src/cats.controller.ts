2409ff68b7f8b990e214fecb07f35c8013872149
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-07-31 10:09:46 -0500

    Update Get and Post routes

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,25 +1,24 @@
-import { Controller, Get, Req, Post, HttpCode, Header, Param, Body } from "@nestjs/common";
+import { Controller, Get, Req, Post, Query, Param, Body } from "@nestjs/common";
 import { Request } from "express";
-import { CreateCatDto } from "./create-cat.dto";
+import { CreateCatDto } from "./dto/create-cat.dto";
+import { ListAllEntities } from "./dto/list-all-entities.dto";
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  @HttpCode(204)
-  @Header('Cache-Control', 'none')
   //we're using a JS class to type this DTO (Data Transfer Object) instead of a TS interface because the js class will be available throughout the life of the program, and interfaces are removed during the transpilation so Nest cannot refer to them at run time. There are some possiblities Pipes can give us if they have access to the metatype of the variable at runtime.
-  async create(@Body() createCatdto: CreateCatDto) {
+  async create(@Body() createCatDto: CreateCatDto) {
     return 'this action creates a new cat';
   }
 
   @Get()
-  async findAll(@Req() request: Request): Promise<any[]> {
-    return ['This action returns all cats'];
+  async findAll(@Query() query: ListAllEntities) {
+    return `This action returns all cats (limit: ${query.limit})`;
   }
 
   @Get(':id')
-  findOne(@Param() params:any): string {
-    return `this action returns cat #${params.id}`
+  findOne(@Param('id') id: string) {
+    return `this action returns cat #${id}`
   }
 
 }
