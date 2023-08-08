c0d40815fe10098bfe633adcc522a93f2b20429f
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-08-08 11:48:42 -0500

    Rewrite controller to utilize response object and add passthrough

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats.controller';
 import { NestDocsController } from './nest-docs/nest-docs.controller';
-import { AdminControllerController } from './admin-controller/admin.controller';
+import { AdminController } from './admin-controller/admin.controller';
 import { AccountController } from './account/account.controller';
 
 //the root module for the application
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, NestDocsController, AdminControllerController, AccountController],
+  controllers: [AppController, CatsController, NestDocsController, AdminController, AccountController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Get, Post, Query, Param, Body, Put, Delete } from "@nestjs/common";
-import { CreateCatDto } from "./dto/create-cat.dto";
+import { Controller, Get, Post, Query, Param, Body, Put, Delete, Res, HttpStatus } from "@nestjs/common";
 import { ListAllEntities } from "./dto/list-all-entities.dto";
 import { UpdateCatDto } from "./dto/update-cat.dto";
+import { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Post()
   //we're using a JS class to type this DTO (Data Transfer Object) instead of a TS interface because the js class will be available throughout the life of the program, and interfaces are removed during the transpilation so Nest cannot refer to them at run time. There are some possiblities Pipes can give us if they have access to the metatype of the variable at runtime.
-  async create(@Body() createCatDto: CreateCatDto) {
-    return 'this action creates a new cat';
+  async create(@Res() res: Response) {
+    res.status(HttpStatus.CREATED).send();
   }
 
   @Get()
-  async findAll(@Query() query: ListAllEntities) {
-    return `This action returns all cats (limit: ${query.limit})`;
+  async findAll(@Res({passthrough: true}) res: Response) {
+    res.status(HttpStatus.OK).json([]);
   }
 
   @Get(':id')
